419e26c2556c52bfef3def165f020c87aa41913a
lucas quivron <lucas.quivron@gmail.com>
2016-08-09 16:05:54 +0200

    Task Manager

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Task.create(name: 'Luke', detail: "I love you baby")
+Task.create(name: 'Jhohn', detail: "Yolo Resto")
+
